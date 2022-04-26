9e64f96b4bb3d0db032dc6ed324e953ecf2543ca
Stan <stantonmoore42@gmail.com>
2022-04-25 21:18:46 -0400

    Created CRUD Routes

---
+++ src/services/notes-api.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const URL = 'https://stan-notes-api.herokuapp.com/notes/'
+
+// Get all posts
+export const getNotes = () => {
+  return axios.get(URL)
+}
+
+// Get one post
+export const getNote = (id) => {
+  return axios.get(`${URL}${id}`)
+}
+
+// Create post
+export const createNote = (createdNote) => {
+  return axios.post(`${URL}`, createdNote)
+}
+
+// Delete post
+export const deleteNote = (id) => {
+  return axios.delete(`${URL}${id}`)
+}
+
+// Update post
+export const editNote = (id, updatedNote) => {
+  return axios.put(`${URL}${id}`, updatedNote)
+}
