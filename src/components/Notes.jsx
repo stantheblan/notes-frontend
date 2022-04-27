5359d3c33cbb3f40ef57b72c702b42d20d548e68
Stan <stantonmoore42@gmail.com>
2022-04-26 23:59:14 -0400

    Create Notes.jsx

---
+++ src/components/Notes.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import { getNotes } from '../services/notes-api.js'
+
+export function Notes() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getNotes()
+      .then(res => setData(res.data))
+  }, [data])
+
+  return (
+    <div className='blog'>
+      <h1>Where's Waldo</h1><br />
+      <div className='notesContainer'>
+        {
+          data.map((e, i) => {
+            return (
+              <div key={i} className='homeNote'>
+                <h2><a href={`/${e._id}`}>{e.title}</a></h2>
+                <p>{e.body}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
