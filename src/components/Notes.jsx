752d7a5f283f566a0488d729bb6cf34ca04b325e
Stan <stantonmoore42@gmail.com>
2022-04-28 19:31:13 -0400

    Display (x,y) of note

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,13 +1,19 @@
 import { useState, useEffect } from 'react'
-import { getNotes } from '../services/notes-api.js'
+import { getNotes, editNote } from '../services/notes-api.js'
 import Draggable from 'react-draggable'
 
 export function Notes() {
   const [data, setData] = useState([]);
+  const [position, setPosition] = useState({ x: 250, y: 150 });
+
   useEffect(() => {
     getNotes()
       .then(res => setData(res.data))
   }, [data])
+
+  const updatePos = (e) => {
+    setPosition({ x: e.x, y: e.y })
+  }
 
   return (
     <div className='blog'>
@@ -17,10 +23,11 @@ export function Notes() {
         {
           data.map((e, i) => {
             return (
-              <Draggable> 
+              <Draggable onStop={(e, data) => updatePos(data)}> 
                 <div key={i} className='homeNote'>
                   <h2><a href={`/${e._id}`}>{e.title}</a></h2>
                   <p>{e.body}</p>
+                  x: {position.x.toFixed(0)}, y: {position.y.toFixed(0)}
                 </div>
               </Draggable>
             )
