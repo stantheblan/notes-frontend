6d116a8db389be2db2308ef76ad9f1193de4d716
Stan <stantonmoore42@gmail.com>
2022-04-27 21:07:50 -0400

    Create New.jsx

---
+++ src/components/New.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom'
+import { createNote } from '../services/notes-api'
+
+export function New() {
+  let nav = useNavigate()
+  const addNewNote = (e) => {
+    e.preventDefault()
+    let note = {
+      title: e.target.title.value,
+      body: e.target.body.value
+    }
+    createNote(note)
+    nav('/')
+  }
+  return (
+    <div className='note'>
+      <h1>New Note:</h1>
+      <form onSubmit={addNewNote}>
+        Title <input type='text' name='title' />
+        <br/><br/>
+        <textarea name='body' cols='40' rows='10'></textarea><br />
+        <input type='submit' /> 
+      </form> <br/>
+    </div>
+  )
+}
