5d8f8bba262f987be9edf2bde324c500e534e11a
Stan <stantonmoore42@gmail.com>
2022-05-01 19:48:07 -0400

    Removing Routes
    moving toward single page application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Notes } from './components/Notes';
-import { New } from './components/New';
-import { Show } from './components/Show';
-import { Edit } from './components/Edit';
 
 export default function App() {
   return (
     <div className="App">
       <Router>
-        <nav>
-          <Link to='/'> ~Home </Link>
-          <Link to='/new'> ~New</Link>
-        </nav>
         <Routes>
           <Route path='/' element={<Notes />}></Route>
-          <Route path='/:id' element={<Show />}></Route>
-          <Route path='/new' element={<New />}></Route>
-          <Route path='/:id/edit' element={<Edit />}></Route>
         </Routes>
       </Router>
     </div>
