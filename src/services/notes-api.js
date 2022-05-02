1afeaac811a4a854883e39bd9377d1670ec4811b
Stan <stantonmoore42@gmail.com>
2022-05-01 22:35:47 -0400

    add 3rd party API call

--- src/services/notes-api.js
+++ src/services/notes-api.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 const URL = 'http://localhost:3001/notes/'
 
+const porkURL = 'https://baconipsum.com/api/?type=pork'
+
 // Get all posts
 export const getNotes = () => {
   return axios.get(URL)
@@ -10,6 +12,11 @@ export const getNotes = () => {
 // Get one post
 export const getNote = (id) => {
   return axios.get(`${URL}${id}`)
+}
+
+// Get films
+export const getPork = () => {
+  return axios.get(`${porkURL}`)
 }
 
 // Create post
